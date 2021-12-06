c7a4e17ebad20b4f7919c229c0e879d94958d190
Leif Guillermo <leif.guillermo@ruralsourcing.com>
2021-12-06 16:09:15 -0700

    day6 - fixed initial setup

--- daily-challenges/src/main/java/com/guillermo/leif/challenges/dec06/Dec06.java
+++ daily-challenges/src/main/java/com/guillermo/leif/challenges/dec06/Dec06.java
@@ -1,35 +1,38 @@
 package com.guillermo.leif.challenges.dec06;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.guillermo.leif.challenges.Challenge;
-import com.guillermo.leif.challenges.dec05.Challenge5;
-import com.guillermo.leif.challenges.dec05.OceanFloor;
-import com.guillermo.leif.inputReaders.xyGraphReader.XyGraphReader;
+import com.guillermo.leif.inputReaders.lanternFishReader.LanternFishReader;
 
 import lombok.extern.slf4j.Slf4j;
 
 
 @Slf4j
 @Component
-@Challenge5
+@Challenge6
 public class Dec06 implements Challenge {
 
+	private LanternFishReader lanternFishReader;
 
+	@Autowired
+	public Dec06(LanternFishReader lanternFishReader) {
+		this.lanternFishReader = lanternFishReader;
+	}
 
-    @Autowired
-    public Dec06(XyGraphReader xyGraphReader, OceanFloor oceanFloor) {
+	@Override
+	public void solveProblem() throws Exception {
+		final String filePath = "daily-challenges/src/main/resources/inputFiles/day6/puzzleInput.txt";
+		final String sampleFilePath = "daily-challenges/src/main/resources/inputFiles/day6/sample1.txt";
 
-    }
+		List<Integer> ages = lanternFishReader.getLanternFishAges(sampleFilePath);
 
-    @Override
-    public void solveProblem() throws Exception {
-        final String filePath = "daily-challenges/src/main/resources/inputFiles/day6/puzzleInput.txt";
-        final String sampleFilePath = "daily-challenges/src/main/resources/inputFiles/day6/sample1.txt";
-
-
-    }
+		log.info("inputAges: " + Arrays.toString(ages.toArray()));
+	}
 
 
 }
--- input-readers/src/main/java/com/guillermo/leif/inputReaders/lanternFishReader/LanternFishReader.java
+++ input-readers/src/main/java/com/guillermo/leif/inputReaders/lanternFishReader/LanternFishReader.java
@@ -2,6 +2,8 @@ package com.guillermo.leif.inputReaders.lanternFishReader;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.springframework.stereotype.Component;
 
@@ -11,6 +13,8 @@ import com.guillermo.leif.inputReaders.AllLineReader;
 public class LanternFishReader extends AllLineReader {
 
 	public List<Integer> getLanternFishAges(String fileName) throws IOException {
-		return readAllLinesFromFileAsIntegers(fileName);
+		List<String> lines = readAllLinesFromFile(fileName);
+		return Stream.of(lines.get(0).split(",")).map(Integer::parseInt).collect(Collectors.toList());
 	}
+
 }
