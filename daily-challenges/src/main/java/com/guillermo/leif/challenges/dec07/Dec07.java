200a11224d5c4a6e24c60bbf449c024b649cedef
Leif Guillermo <guillermoleif@gmail.com>
2021-12-07 08:25:15 -0700

    day7 - finish day 7

--- daily-challenges/src/main/java/com/guillermo/leif/challenges/dec07/Dec07.java
+++ daily-challenges/src/main/java/com/guillermo/leif/challenges/dec07/Dec07.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.NoSuchElementException;
 
 
 @Slf4j
@@ -28,52 +28,48 @@ public class Dec07 implements Challenge {
         final String filePath = "daily-challenges/src/main/resources/inputFiles/day7/puzzleInput.txt";
         final String sampleFilePath = "daily-challenges/src/main/resources/inputFiles/day7/sample1.txt";
 
-        List<Integer> crabLocations = crabLocationReader.getCrabLocations(sampleFilePath);
+        List<Integer> crabLocations = crabLocationReader.getCrabLocations(filePath);
         log.info("Crab locations: " + Arrays.toString(crabLocations.toArray()));
 
-        int locationsSize = crabLocations.size();
-        int halfSize = locationsSize / 2;
-
-        List<Integer> result = findShortestDistance(crabLocations.subList(0, halfSize), crabLocations.subList(halfSize, locationsSize));
-
-        result = result.stream().sorted().collect(Collectors.toList());
+        findSmallestFuelCost(crabLocations);
 
     }
 
-    private List<Integer> findShortestDistance(List<Integer> firstHalf, List<Integer> secondHalf) {
-        int firstHalfSize = firstHalf.size();
-        int halfFirstHalf = firstHalfSize / 2;
+    private void findSmallestFuelCost(List<Integer> crabs) {
+        int min = crabs.stream().mapToInt(i -> i).min().orElseThrow(NoSuchElementException::new);
+        int max = crabs.stream().mapToInt(i -> i).max().orElseThrow(NoSuchElementException::new);
 
-        int secondHalfSize = secondHalf.size();
-        int halfSecondHalf = secondHalfSize / 2;
+        int currentMinFuel = Integer.MAX_VALUE;
+        int minPoint = -1;
 
-        if (firstHalfSize == 1 && secondHalfSize > 1) {
-            return List.of(
-                    (firstHalf.get(0) + findShortestDistance(
-                            secondHalf.subList(0, halfSecondHalf),
-                            secondHalf.subList(halfSecondHalf, secondHalfSize)).get(0))
-                            / 2);
+        for (int point = min; point < max; point++) {
+//            int currentFuelCost = countTotalFuelCostToPoint(point, crabs);
+            int currentFuelCost = countTotalFuelCostToPointAccumulated(point, crabs);
+            if (currentFuelCost < currentMinFuel) {
+                currentMinFuel = currentFuelCost;
+                minPoint = point;
+            }
         }
 
-        if (secondHalfSize == 1 && firstHalfSize > 1) {
-            return List.of(
-                    (secondHalf.get(0) + findShortestDistance(
-                            firstHalf.subList(0, halfFirstHalf),
-                            firstHalf.subList(halfFirstHalf, firstHalfSize)).get(0))
-                            / 2);
+        log.info("Min Fuel: " + currentMinFuel);
+        log.info("Min Point: " + minPoint);
+    }
+
+    private int countTotalFuelCostToPointAccumulated(int point, List<Integer> crabs) {
+        int totalFuel = 0;
+
+        for (int crabLocation : crabs) {
+            int distance = Math.abs(crabLocation - point);
+            for (int i = 1; i <= distance; i++) {
+                totalFuel += i;
+            }
         }
 
-        if (firstHalfSize == 1 && secondHalfSize == 1) {
-            return List.of((firstHalf.get(0) + secondHalf.get(0)) / 2);
-        }
+        return totalFuel;
+    }
 
-        List<Integer> firstHalfA = firstHalf.subList(0, halfFirstHalf);
-        List<Integer> firstHalfB = firstHalf.subList(halfFirstHalf, firstHalfSize);
-        List<Integer> secondHalfA = secondHalf.subList(0, halfSecondHalf);
-        List<Integer> secondHalfB = secondHalf.subList(halfSecondHalf, secondHalfSize);
-
-        return findShortestDistance(findShortestDistance(firstHalfA, firstHalfB), findShortestDistance(secondHalfA, secondHalfB));
-
+    private int countTotalFuelCostToPoint(int point, List<Integer> crabs) {
+        return crabs.stream().reduce(0, (fuelCost, crab) -> fuelCost += Math.abs(point - crab));
     }
 
 }
